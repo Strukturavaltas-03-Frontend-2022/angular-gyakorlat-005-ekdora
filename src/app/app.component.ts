a51a098a4bf480dcd051d55e4641dc34b6a8eb03
ekdora <dora.egrykiss@gmail.com>
2022-10-31 17:28:36 +0100

    Task solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './model/user';
 import { UserService } from './service/user.service';
 
@@ -7,9 +7,27 @@ import { UserService } from './service/user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The good Angular programmer';
+  users: User[] = [];
+  currentUser: User = {} as User;
 
-  constructor() {}
+  constructor(private userSvc: UserService) {
+  }
 
+  ngOnInit(): void {
+    this.users = this.userSvc.list;
+  }
+
+  onSelectClicked(user: User) {
+    this.currentUser = user;
+  }
+
+  onUpdateClicked(user: User) {
+    this.userSvc.updateUser(user);
+  }
+
+  onDeleteClicked(user: User) {
+    this.userSvc.removeUser(user);
+  }
 }
